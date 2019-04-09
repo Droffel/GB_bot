47bcf03d9d22c118433c1b932b20389b7a9d8910
Droffel <49450858+Droffel@users.noreply.github.com>
2019-04-09 19:52:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ const Discord = require("discord.js");
 
 const Token = "NTY0NTA3MjQyMTMzNzE3MDAy.XKujTg.4aNQD2hNa9E9vvC3606nlN3NIjo";
 
+const Prefix = "T"
+
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
-  console.log("ready")
+  console.log("ready");
 });
+
 
 bot.on("message", function(message) {
   console.log(message.content);
+  if (message.author.equals(bot.user)) return;
+
+  if (!message.content.startsWith(Prefix)) return;
+
+  var args = message.content.substring(Prefix.length).split(" ");
+
+  switch (args[0].toLowerCase()) {
+    case "ping":
+    message.channel.sendMessage("Pong!");
+    break;
+  }
 });
 
 bot.login(process.env.Token);
