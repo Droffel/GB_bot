a625daa17e374fb43ce5ca68cd30cc97bbf7f4ab
Droffel <49450858+Droffel@users.noreply.github.com>
2019-04-09 19:26:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const Token = "NTY0NTA3MjQyMTMzNzE3MDAy.XKujTg.4aNQD2hNa9E9vvC3606nlN3NIjo";
 
 var bot = new Discord.Client();
 
+bot.on("ready", function() {
+  console.log("ready")
+});
+
 bot.on("message", function(message) {
   console.log(message.content);
 });
