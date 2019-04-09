703609c35697b8cf93b30e64d2a095271b6688fc
Droffel <49450858+Droffel@users.noreply.github.com>
2019-04-09 18:54:20 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+const Token = "NTY0NTA3MjQyMTMzNzE3MDAy.XKujTg.4aNQD2hNa9E9vvC3606nlN3NIjo";
+
+var bot = new Discord.Client();
+
+bot.on("message", function(message) {
+  console.log(message.content);
+});
+
+bot.login(process.env.Token);
